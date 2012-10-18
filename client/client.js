82557bbb77570e22e11fd65d6170614dfbbff1c4
Lyndon Maydwell <lyndon@sordina.net>
2012-10-18 23:51:15 +0800

    Updated parsing

--- client/client.js
+++ client/client.js
@@ -8,51 +8,29 @@ Lists.insert( {
 	          ]
 } )
 
-function find(namedList, name) {
-	if(namedList.name == name) { return namedList }
-
-	return _.chain(namedList.children || [])
-	        .map(  function(e){ return find(e,name) })
-	        .find( function(e){return e})
-	        .value()
-}
-
-function remove(namedList, name) {
-	if(namedList.name == name) { throw("Could not perform this odd operation.") }
-	var removed = _.reject(namedList.children || [], function(e){ return e.name == name })
-	var pruned  = _.map(removed, remove)
-	return {name: namedList.name, children: pruned}
-}
-
-function insert(namedList, item, name, index) {
-	if(name ==  namedList.name)
-	{
-		namedList.children = namedList.children || []
-		namedList.children.splice(index, 0, item);
-	}
-	else
-		_.each(namedList.children || [], function(e) {insert(e,item,name,index)})
+Template.initial.lists = function () {
+	return Lists.find()
 }
 
 Template.main.children = function () {
-	return this.name ? (this.children || []) : Lists.find();
+	return this.children || []
 }
 
 Template.main.rendered = function () {
-
 	$(function() {
 		$( "ul" ).sortable({
 			connectWith: "ul",
 			update: function(e,o){
-				var name    = $(o.item[0]).find('h2').text()
-				var parent  = $(o.item[0]).parent().closest('li').children('h2').text()
-				var list    = Lists.findOne({name: "Item 1"})
-				var removed = find(list, name)
-				var pruned  = remove(list, name)
-				insert(pruned, removed, parent, 0)
-				Lists.update({name: "Item 1"}, pruned)
+				var updated = parse($('body'))
+				Lists.update({name: "Item 1"}, {'$set' : updated})
 			}
-		}).disableSelection();
-	});
+		}).disableSelection()
+	})
+}
 
+function parse(jitem) {
+	return {
+		name     : jitem.children('h2').text(),
+		children : jitem.find(' > ul > li').map(function(i,e){return parse($(this))}).get()
+	}
 }
